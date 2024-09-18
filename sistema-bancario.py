4a25cec456056566c7533bd7696caa936e39ef03
edu510 <www.eduardo.f.n@gmail.com>
2024-09-18 00:55:06 -0300

    refactor: Separa a operacao de saque em uma funcao

--- sistema-bancario.py
+++ sistema-bancario.py
@@ -28,6 +28,33 @@ def exibir_extrato(dict):
     for tempo, operacao in dict.items():
         print(f"{tempo:%d-%m-%Y %H:%M:%S} - {operacao}")
 
+def sacar(*, valor, saldo, limite, numero_saques, limite_saques):
+    
+    excedeu_saldo = valor > saldo
+    excedeu_limite = valor > limite
+    excedeu_saques = numero_saques >= limite_saques
+
+    if excedeu_saldo:
+        print("Operação falhou! Você não tem saldo suficiente.")
+
+    elif excedeu_limite:
+        print("Operação falhou! O valor do saque excede o limite.")
+
+    elif excedeu_saques:
+        print("Operação falhou! Número máximo de saques excedido.")
+
+    elif valor <= 0:
+        print("Operação falhou! O valor informado é inválido.")
+
+    else:
+        saldo -= valor
+        adiciona_operacao_com_tempo(dict_tempo_operacao, f"Saque: R$ {valor:.2f}")
+        numero_saques += 1
+        print(f"Saque de R${valor:.2f} realizado")
+        print("Saldo atualizado: " + str(saldo))
+    return
+
+
 while True:
 
     opcao = input(menu).lower()
@@ -42,39 +69,20 @@ while True:
 
             else:
                 print("Operação falhou! O valor informado é inválido.")
+
         case "s" | "sacar":
             valor = float(input("Informe o valor do saque: "))
-
-            excedeu_saldo = valor > saldo
-
-            excedeu_limite = valor > limite
-
-            excedeu_saques = numero_saques >= LIMITE_SAQUES
-
-            if excedeu_saldo:
-                print("Operação falhou! Você não tem saldo suficiente.")
-
-            elif excedeu_limite:
-                print("Operação falhou! O valor do saque excede o limite.")
-
-            elif excedeu_saques:
-                print("Operação falhou! Número máximo de saques excedido.")
-
-            elif valor > 0:
-                saldo -= valor
-                adiciona_operacao_com_tempo(dict_tempo_operacao, f"Saque: R$ {valor:.2f}")
-                numero_saques += 1
-
-            else:
-                print("Operação falhou! O valor informado é inválido.")
+            sacar(valor=valor, saldo=saldo, limite=limite, numero_saques=numero_saques, limite_saques=LIMITE_SAQUES)
 
         case "e" | "extrato":
             print("\n================ EXTRATO ================")
             print("Não foram realizadas movimentações.") if not dict_tempo_operacao else exibir_extrato(dict_tempo_operacao)
-            print(f"\nSaldo: R$ {saldo:.2f}")
+            print(f"\nSaldo: R$ {saldo:%.2f}")
             print("==========================================")
+        
         case "q" | "sair":
             break
+        
         case _:
             print("Operação inválida, por favor selecione novamente a operação desejada.")
 
